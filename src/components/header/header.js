909e993d1844c552e34370e5d095a2b4df63fc9c
Maxim <bevzmaxim@ukr.net>
2021-06-15 08:48:42 +0300

    remove folder

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,7 +4,6 @@ import NavMenu from '../nav-menu/nav-menu';
 import ChangeMode from '../change-mode/change-mode';
 import Cabinet from '../cabinet/cabinet';
 import Logo from '../logo/logo';
-import {useState} from 'react';
 
 export default function Header({activeMode}) {
 
